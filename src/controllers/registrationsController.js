d08b426b96e399060d7a991758ad27757baece9e
Paulo <marquespaulov@gmail.com>
2020-12-13 00:54:06 -0300

    overview and hotel

--- src/controllers/registrationsController.js
+++ src/controllers/registrationsController.js
@@ -25,4 +25,38 @@ router.post("/create", validateRegistration, authMiddleware, async (req, res) =>
     }
 })
 
+router.post("/personal-info", authMiddleware, async (req, res) => {
+    const userId = res.locals.user.id;
+
+    try{
+        let newPersonalInfo = await registrationsRepository.updatePersonalInfo(userId, req.body);
+        res.status(201).send(newPersonalInfo)
+    }catch(err){
+        console.log(err)
+    }
+})
+
+router.get("/hotel", authMiddleware, async (req,res) => {
+    const userId = res.locals.user.id;
+    
+    try{
+        let hotelRegistrated = await registrationsRepository.getHotel(userId);        
+        res.status(200).send(hotelRegistrated)
+    }catch(err){
+        console.log(err)
+    }
+})
+
+router.post("/hotel", authMiddleware, async (req, res) => {
+    const userId = res.locals.user.id;
+    const { hotel } = req.body;
+
+    try{
+        let newHotel = await registrationsRepository.updateHotel(userId,hotel);
+        res.status(201).send(newHotel) 
+    }catch(err){
+        console.log(err)
+    }
+});
+
 module.exports = router;
--- src/repositories/registrationsRepository.js
+++ src/repositories/registrationsRepository.js
@@ -19,6 +19,25 @@ async function create(userId, registrationParams) {
   return newRegistration;
 }
 
+async function updatePersonalInfo(userId, registrationParams) {
+  const {address, phone} = registrationParams;
+
+  const resp = await db.query('UPDATE registrations SET (address, phone)=($1, $2) WHERE "userId"=$3 RETURNING *', [
+    address,
+    phone,
+    userId,    
+  ]);
+  return resp.rows[0];
+}
+
+async function updateHotel(userId, hotel) {
+  const resp = await db.query('UPDATE hotels SET hotel=$1 WHERE "userId"=$2 RETURNING *', [
+    hotel,
+    userId,    
+  ]);
+  return resp.rows[0];
+}
+
 async function setHotel(userId, hotel) {
   await db.query('INSERT INTO hotels ("userId", hotel) VALUES ($1, $2)', [
     userId,
@@ -41,4 +60,4 @@ async function getUserRegistration(userId) {
   return resp.rows[0];
 }
 
-module.exports = { create, setHotel, getUserRegistration, getHotel };
+module.exports = { create, setHotel, getUserRegistration, getHotel, updateHotel, updatePersonalInfo };
