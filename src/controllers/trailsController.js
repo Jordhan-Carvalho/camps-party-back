0792a2581b230c710ee90fe7165a5c227ed9f7eb
Paulo <marquespaulov@gmail.com>
2020-12-13 16:26:37 -0300

    wip: update trails

--- src/controllers/trailsController.js
+++ src/controllers/trailsController.js
@@ -5,15 +5,26 @@ const authMiddleware = require('../middlewares/auth');
 const router = express.Router();
 
 router.post("/post-trails", authMiddleware, async (req, res) => {
+  const userId = res.locals.user.id;
+  const trails = req.body;
 
-    const trails = req.body;
-    
-    try {
-      const createdTrail = await trailsRepository.create(res.locals.user.id, trails);
-      res.status(201).send(createdTrail);
-    } catch (e) {
-      res.status(409).send(e.message);
-    }
+  try {
+    const createdTrail = await trailsRepository.create(userId, trails);
+    res.status(201).send(createdTrail);
+  } catch (e) {
+    res.status(409).send(e.message);
+  }
   });
+
+router.get("/get-trails", authMiddleware, async(req, res) => {
+  const userId = res.locals.user.id;
+
+  try{
+    const chosenTrails = await trailsRepository.getUserTrails(userId);
+    res.status(200).send(chosenTrails);
+  }catch(err){
+    console.log(err)
+  }
+})
 
   module.exports = router;
